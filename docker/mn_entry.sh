da8d64284e279c8ece395d640a76fc3e2d87d36a
SmartInsider <smartyinsider@gmail.com>
2020-02-25 15:42:24 +0200

    relize

---
+++ docker/mn_entry.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+
+COIN_NAME="$1"
+masternodeprivkey="$2"
+ip=$3
+
+DLINKHOST="https://download.happyleaf.money/wallet/ubuntu18/${COIN_NAME}-light.tar.gz"
+TARDAEMON=${COIN_NAME}-light.tar.gz
+
+
+
+cd /usr/local/bin
+wget -O ${TARDAEMON} ${DLINKHOST}/${TARDAEMON} && tar -xzf ${TARDAEMON} && rm ${TARDAEMON}
+chmod 744 *
+
+
+echo "COIN_NAME=$COIN_NAME"
+echo "masternodeprivkey=$masternodeprivkey"
+echo "ip=$ip"
+
+cd /root/.${COIN_NAME}
+
+confFile="${COIN_NAME}.conf"
+port=5333
+
+echo -e "\e[94m$(date +"%H:%M:%S %d/%m/%Y") Create ${confFile}\e[0m"
+echo "rpcuser=mTr57v3kbj" >> $confFile
+echo "rpcpassword=8p9I6mjzh5luYRuCDEBg1iGmy" >> $confFile
+echo "txindex=1" >> $confFile
+echo "zefirstake=1" >> $confFile
+echo "rpcallowip=127.0.0.1" >> $confFile
+echo "rpcport=17901" >> $confFile
+echo "server=1" >> $confFile
+echo "daemon=0" >> $confFile
+echo "port=${port}" >> $confFile
+echo "masternodeprivkey=$masternodeprivkey" >> $confFile
+echo "masternodeaddr=$ip" >> $confFile
+echo "printtoconsole=1" >> $confFile
+echo "masternode=1" >> $confFile
+echo "bantime=50" >> $confFile
+echo "maxconnections=256" >> $confFile
+
+${COIN_NAME}d --printtoconsole
